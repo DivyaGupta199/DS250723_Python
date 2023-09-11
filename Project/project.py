c49e931c613baa5eb0affcaa3f266b297c6f58e7
DivyaGupta199 <117897340+DivyaGupta199@users.noreply.github.com>
2023-09-11 19:56:59 +0530

    Update project.py

---
+++ Project/project.py
@@ -0,0 +1,17 @@
+# Project description : 
+# online Bookstore : 
+'''you need to make a website for a bookstore, it will have a login page for admin
+and one for the customer perspective. for the admin view we will have following features :
+1. Admin login
+2. Add a Book
+3. Update a Book detail
+4. Delete a Book
+
+from the customer propective, we will have following features : 
+1. login/signup page
+2. view books
+3. Add books to the cart
+4. Order books
+5. view order history
+6. update profile
+'''
